f33c0ae72a6ac249cd9760dc4d8b12d63dc861c2
vladislav <vladnovoren@gmail.com>
2022-04-15 11:22:46 +0300

    dot product

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,43 +1,48 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+
 #include <cstddef>
 
-template<typename ElemT, size_t Dim>
-class Vector {
- public:
-  template<typename... Args>
-  Vector(Args... args) : components{args...} {
+template <typename ElemT, size_t Dim>
+class Vector;
+
+template <size_t index, typename ElemT, size_t Dim>
+struct Dotter {
+  static ElemT DotProductImpl(const Vector<ElemT, Dim> &lhs,
+                              const Vector<ElemT, Dim> &rhs) {
+    return lhs[index] * lhs[index] +
+           Dotter<index - 1, ElemT, Dim>::DotProductImpl(lhs, rhs);
+  }
+};
+
+template <typename ElemT, size_t Dim>
+struct Dotter<0, ElemT, Dim> {
+  static ElemT DotProductImpl(const Vector<ElemT, Dim> &lhs,
+                              const Vector<ElemT, Dim> &rhs) {
+    return lhs[0] * lhs[0];
+  }
+};
+
+template <typename ElemT, size_t Dim> class Vector {
+public:
+  template <typename... Args> Vector(Args... args) : components{args...} {
     static_assert(sizeof...(args) == Dim, "Wrong dimension");
   }
 
-  ElemT& operator[](const size_t index) {
-    return components[index];
+  ElemT &operator[](const size_t index) { return components[index]; }
+
+  const ElemT &operator[](const size_t index) const {
+    return const_cast<Vector<ElemT, Dim> *>(this)->operator[](index);
   }
 
-  const ElemT& operator[](const size_t index) const {
-    return const_cast<Vector<ElemT, Dim>*>(this)->operator[](index);
+  ElemT operator*(const Vector &other) {
+    return Dotter<Dim - 1, ElemT, Dim>::DotProductImpl(*this, other);
   }
 
-  ElemT operator*(const Vector& other) {
-    return Dotter<Dim - 1>::DotProductImpl(*this, other);
-  }
-
- private:
+private:
   static_assert(Dim != 0);
 
-  template<size_t index>
-  struct Dotter {
-    static ElemT DotProductImpl(const Vector<ElemT, Dim>& a, const Vector<ElemT, Dim>& b) {
-      return a[index] * b[index] + Dotter<index - 1>::DotProductImpl(a, b);
-    }
-  };
-
   ElemT components[Dim] = {};
-
 };
 
-template<typename ElemT, size_t Dim>
-template<>
-struct Vector<ElemT, Dim>::Dotter<0> {
-  static ElemT DotProductImpl(const Vector<ElemT, Dim>& a, const Vector<ElemT, Dim>& b) {
-    return a[0] * b[0];
-  }
-};
+#endif /* vector.hpp */
